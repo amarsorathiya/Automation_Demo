28d12e47e114e2dbd4daf0e4ad5d4c3a3caa4e32
amarsorathiya <sorathiyaamar@gmail.com>
2020-04-05 15:05:19 +0530

    Fist Project added

---
+++ demo/src/test/java/WinniumTest.java
@@ -0,0 +1,50 @@
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.winium.DesktopOptions;
+import org.openqa.selenium.winium.WiniumDriver;
+import org.openqa.selenium.winium.WiniumDriverService;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class WinniumTest {
+	
+	 static WiniumDriver driver = null;
+	    static WiniumDriverService service = null;
+	    static DesktopOptions options = null;
+	
+	@BeforeClass
+	private void SetUp() {
+	    options = new DesktopOptions(); //Instantiate Winium Desktop Options
+	    options.setApplicationPath("C:\\Windows\\System32\\calc.exe");
+	    File driverPath = new File("E:\\Winium.Desktop.Driver\\Winium.Desktop.Driver.exe");
+	    System.setProperty("webdriver.winium.desktop.driver","E:\\Winium.Desktop.Driver\\Winium.Desktop.Driver.exe");
+	    service = new WiniumDriverService.Builder().usingDriverExecutable(driverPath).usingPort(9999).withVerbose(true)
+	            .withSilent(false).buildDesktopService();
+	    try {
+	        service.start();
+	    } catch (IOException e) {
+	        System.out.println("Exception while starting WINIUM service");
+	        e.printStackTrace();
+	    }
+	}
+	
+	
+	@AfterClass
+	private void tearDown() {
+		driver.quit();
+		
+	}
+	
+	@Test
+	private void demoTest() {
+		
+		
+	}
+	
+	
+
+}
